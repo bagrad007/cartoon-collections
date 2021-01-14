42542c1a7538389e0cea58c5695ea899b7579313
Luke Gill <bagrad007@gmail.com>
2021-01-14 15:52:54 -0500

    commit tree cc7900210a4f346d71790b65ae9273093c14e06d

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(dwarves)# code an argument here
+  string = ""
+  dwarves.map.with_index(1) do |dwarf, index|
+    string += "#{index}. *#{dwarf}"
+  end
+puts string
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)# code an argument here
+  planeteers.map do |planeteer| 
+    "#{planeteer.capitalize}!"
+  end
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)# code an argument here
+    words.find do |word|
+      if word.length < 4
+        return true
+      end
+      return false
+    end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arrays)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arrays.find do |array|
+    # binding.pry
+    if cheese_types.include?("#{array}")
+     return array
+     
+    end
+  end
 end
